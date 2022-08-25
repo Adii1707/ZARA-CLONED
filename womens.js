8b93e574d7939af10e42fdcb210b0f6abdc50ad0
Aditya Singh Ghosh <89689829+Adii1707@users.noreply.github.com>
2022-08-25 23:20:46 +0530

    Add files via upload

--- womens.js
+++ womens.js
@@ -1,13 +1,19 @@
 
-let womensdata = [
+var womensdata = [
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/390/2/w/257/7885138390_15_14_1.jpg?ts=1660818002119",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/390/2/w/750/7885138390_2_3_1.jpg?ts=1661181016023",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/390/2/w/750/7885138390_2_6_1.jpg?ts=1661181017496",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/390/2/w/750/7885138390_2_2_1.jpg?ts=1661181015391",
         name: "SATIN OVERSIZE SHIRT",
         price: 2890.00
     },
 
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/085/2/w/257/7885138085_15_14_1.jpg?ts=1660817999473",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/330/2/w/750/7885138330_1_1_1.jpg?ts=1654672970804",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/330/2/w/750/7885138330_6_1_1.jpg?ts=1655127102649",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/7885/138/330/2/w/750/7885138330_6_2_1.jpg?ts=1655127119602",
         name: "SATIN OVERSIZE SHIRT",
         price:  2890.00
 
@@ -15,11 +21,17 @@ let womensdata = [
 
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/1971/176/629/2/w/257/1971176629_1_1_1.jpg?ts=1660900615181",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/1971/176/629/2/w/750/1971176629_2_1_1.jpg?ts=1660900605030",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/1971/176/629/2/w/750/1971176629_2_3_1.jpg?ts=1660900604629",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/1971/176/629/2/w/750/1971176629_2_5_1.jpg?ts=1660900607322",
         name: "DOUBLE RUFFLE FLOWING SHIRT",
         price:  2890.00
     },
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/4437/048/600/3/w/563/4437048600_2_3_1.jpg?ts=1661161880896",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/4437/048/600/3/w/563/4437048600_2_1_1.jpg?ts=1661161882827",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/4437/048/600/3/w/750/4437048600_1_1_1.jpg?ts=1661161883292",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/4437/048/600/3/w/750/4437048600_2_2_1.jpg?ts=1661161882705",
         name: "SATIN SHIRT",
         color: "orange",
         price: 2990.00
@@ -27,36 +39,57 @@ let womensdata = [
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/3666/188/644/2/w/257/3666188644_2_3_1.jpg?ts=1660900618653",
         name: "POPLIN SHIRT WITH BEJEWELLED BUTTON",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/3666/188/644/2/w/750/3666188644_2_1_1.jpg?ts=1660900619288",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/3666/188/644/2/w/750/3666188644_2_2_1.jpg?ts=1660900628303",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/3666/188/644/2/w/750/3666188644_2_3_1.jpg?ts=1660900618653",
         price: 2990.00
     },
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/3564/196/629/2/w/257/3564196629_1_1_1.jpg?ts=1660900626304",
         name: "PLEATED CAPE SLEEVE BLOUSE",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/3564/196/629/2/w/750/3564196629_2_1_1.jpg?ts=1660900620674",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/3564/196/629/2/w/750/3564196629_2_3_1.jpg?ts=1660900622851",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/3564/196/629/2/w/750/3564196629_2_4_1.jpg?ts=1660900623443",
         price: 2890.00
     },
     {
-        img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
-        name: "POPLIN SHIRT WITH FRENCH CUFFS",
-        price: 2890.00
+        img: "https://static.zara.net/photos///2022/I/0/1/p/8488/731/800/2/w/561/8488731800_2_6_1.jpg?ts=1661353711786",
+        name: "RUFFLED JUMPSUIT WITH BELT",
+        img1: "https://static.zara.net/photos///2022/I/0/1/p/8488/731/800/2/w/750/8488731800_2_1_1.jpg?ts=1661353708969",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/8488/731/800/2/w/750/8488731800_2_3_1.jpg?ts=1661353711799",
+        img3: "https://static.zara.net/photos///2022/I/0/1/p/0387/173/800/2/w/561/0387173800_1_1_1.jpg?ts=1661339651544",
+        price: 5590.00
     },
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/8741/255/030/2/w/257/8741255030_1_1_1.jpg?ts=1660905227873",
         name: "ANIMAL PRINT SHIRT",
+        img1: "",
+        img2: "",
+        img3: "",
         price: 2990.00
     },
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/2492/154/330/2/w/257/2492154330_1_1_1.jpg?ts=1660900609517",
         name: "GEOMETRIC PRINT SHIRT",
+        img1: "",
+        img2: "",
+        img3: "",
         price: 2990.00
     },
     {
         img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
         name: "FITTED LACE SHIRT",
+        img1: "",
+        img2: "",
+        img3: "",
         price: 3990.00
     },
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/4437/048/640/3/w/257/4437048640_2_2_1.jpg?ts=1661178099903",
         name: "SATIN SHIRT",
+        img1: "",
+        img2: "",
+        img3: "",
         price: 2990.00
     },
     {
@@ -150,22 +183,22 @@ let womensdata = [
         price: 1890.00
     },
     {
-        img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        img: "https://static.zara.net/photos///2022/I/0/1/p/8377/201/300/2/w/257/8377201300_2_1_1.jpg?ts=1657527028662",
         name: "BODYSUIT WITH BOW",
         price: 2890.00
     },
     {
-        img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        img: "https://static.zara.net/photos///2022/V/0/1/p/4387/630/712/2/w/313/4387630712_1_1_1.jpg?ts=1643813546779",
         name: "RIBBED BODYSUIT WITH SLOGAN",
         price: 1890.00
     },
     {
-        img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        img: "https://static.zara.net/photos///2022/I/0/1/p/7149/047/430/2/w/313/7149047430_1_1_1.jpg?ts=1655289615649",
         name: "BODYSUIT WITH PLUNGING NECKLINE",
         price: 1790.00
     },
     {
-        img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        img: "https://static.zara.net/photos///2022/I/0/1/p/7901/513/427/2/w/313/7901513427_1_1_1.jpg?ts=1658401097052",
         name: "SATIN BODYSUIT",
         price: 2590.00
     },
@@ -175,9 +208,12 @@ let womensdata = [
         price: 1990.00
     },
     {
-        img: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
-        name: "DRAPED BODYSUIT",
-        price: 1590.00
+        img: "https://static.zara.net/photos///2022/I/0/1/p/2335/006/250/22/w/257/2335006250_2_1_1.jpg?ts=1656416763995",
+        name: "REGULAR FIT HEAVY COTTON T-SHIRT",
+        img1:"https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        img2: "https://static.zara.net/photos///2022/I/0/1/p/2335/006/250/22/w/750/2335006250_2_2_1.jpg?ts=1656416762877",
+        img3: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        price: 990.00
     },
     {
         img: "https://static.zara.net/photos///2022/I/0/1/p/5039/371/800/2/w/257/5039371800_1_1_1.jpg?ts=1656426122630",
@@ -208,10 +244,34 @@ let womensdata = [
 
 ]
 
+console.log(womensdata)
+document.getElementById("sort").addEventListener("change", sort1)
 
+function sort1() {
+  let value =  document.getElementById("sort").value;
+console.log(value)
+ if(value==="lth"){
+    womensdata.sort((a,b)=> {
+      return  a.price-b.price;
+    })
+    redurdom();
+ console.log(womensdata)
 
+ }
+ else{
+    womensdata.sort((a,b)=> {
+       return b.price-a.price;
+       // console.log(a.price)
+    })
+    
+    redurdom();
+ console.log(womensdata)
 
-let redurdom = (womensdata)=> {
+ }
+
+}
+
+let redurdom = ()=> {
     let cont = document.getElementById("container");
     cont.innerHTML = null
 
@@ -231,7 +291,7 @@ let redurdom = (womensdata)=> {
         cont.append(div); 
     })
 }
-redurdom(womensdata);
+redurdom();
 
 let detaildata = (el)=> {
     let data = el;
@@ -239,3 +299,4 @@ let detaildata = (el)=> {
     localStorage.setItem("detaildata", JSON.stringify(data));
     window.location.href = "womensD.html";
 }
+
